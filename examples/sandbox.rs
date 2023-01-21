2b63567f809f177d829de7e6c5bb97b500abd090
damccull <401104+damccull@users.noreply.github.com>
2023-01-21 00:05:23 -0500

    Basic actor worked out

--- examples/sandbox.rs
+++ examples/sandbox.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world.");
+use std::sync::Arc;
+
+use message_broker::Handle;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let handle = Handle::new();
+    handle.publish(Arc::new("Test".to_string())).await?;
+    handle.subscribe().await?;
+
+    Ok(())
+
+    // let mut data = anymap::AnyMap::new();
+    // data.insert(Vec::<String>::new());
+    // data.insert(Vec::<i32>::new());
+
+    // let int = data.get_mut::<Vec<i32>>().unwrap();
+    // for i in 0..10 {
+    //     (*int).push(i);
+    // }
+
+    // let strn = data.get_mut::<Vec<String>>().unwrap();
+    // strn.push("Hello".to_string());
+    // strn.push("World".to_string());
+
+    // let x = data.get::<Vec<i32>>();
+    // let y = data.get::<Vec<String>>();
+    // dbg!(x, y);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,88 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{any::{TypeId, Any}, collections::HashMap, sync::{Arc, mpsc::Sender}};
+
+use tokio::sync::mpsc;
+
+pub struct Actor {
+    receiver: mpsc::Receiver<MessageBrokerControlMessage>,
+    subscriptions: HashMap<TypeId, Vec<Sender<Box<dyn Any + Send + Sync>>>>,
+}
+
+impl Actor {
+    fn new(receiver: mpsc::Receiver<MessageBrokerControlMessage>) -> Self {
+        Self {
+            receiver,
+            subscriptions: HashMap::default(),
+        }
+    }
+
+    fn handle_message(&self, message: MessageBrokerControlMessage) {
+        match message {
+            MessageBrokerControlMessage::Publish(m) => {
+                dbg!(m);
+            }
+            MessageBrokerControlMessage::Subscribe => {
+                dbg!(message);
+            }
+        }
+    }
+}
+
+pub struct Handle {
+    sender: mpsc::Sender<MessageBrokerControlMessage>,
+}
+
+impl Handle {
+    pub fn new() -> Self {
+        let (tx, rx) = mpsc::channel(10);
+        let actor = Actor::new(rx);
+
+        tokio::spawn(run_actor(actor));
+
+        Self { sender: tx }
+    }
+
+    pub async fn publish<MessageType>(&self, message: MessageType) -> Result<(), anyhow::Error>
+    where
+        MessageType: Any + Send + Sync,
+    {
+        let arced = Arc::new(message);
+        let x = self
+            .sender
+            .send(MessageBrokerControlMessage::Publish(arced))
+            .await;
+
+        if x.is_err() {
+            anyhow::bail!("Unable to send the message to the actor: {:?}", x);
+        }
+
+        Ok(())
+    }
+
+    pub async fn subscribe(&self) -> Result<(), anyhow::Error> {
+        let x = self
+            .sender
+            .send(MessageBrokerControlMessage::Subscribe)
+            .await;
+
+        if x.is_err() {
+            anyhow::bail!("Unable to send the message to the actor: {:?}", x);
+        }
+
+        Ok(())
+    }
+}
+
+pub async fn run_actor(mut actor: Actor) {
+    while let Some(msg) = actor.receiver.recv().await {
+        actor.handle_message(msg);
+    }
+}
+
+#[derive(Debug)]
+pub enum MessageBrokerControlMessage {
+    Publish(Arc<dyn Any + Send + Sync>),
+    Subscribe,
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
